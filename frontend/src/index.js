b86b14b08587393344a26495987fca439086fb3c
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2021-01-27 17:53:50 -0300

    renderizando todo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, fas);
 
 ReactDOM.render(
   <React.StrictMode>
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
 import './styles.css';
 
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import {  faPlusCircle as adicionarTarefa, faTrashAlt as lixeira } from '@fortawesome/free-solid-svg-icons';
+import { faCircle as tarefaPendente, faCheckCircle as tarefaConcluida} from '@fortawesome/free-regular-svg-icons';
+
 const Home = props => {
+
+    const tasks = [
+        {
+            state: false,
+            description: 'JOGAR',
+            id: 1
+        },
+        {
+            state: false,
+            description: 'COMER',
+            id: 2
+        },
+        {
+            state: true,
+            description: 'LAVAR LOUCA',
+            id: 3
+        },
+        {
+            state: true,
+            description: 'COZINHAR',
+            id: 4
+        },
+    ];
+
+    const renderTarefas = () => {
+        return (
+            tasks.map((task) => {
+                return (
+                    <li key={task.id}> 
+                    <div className="task">
+                        <div className={!task.state ? 'state' : 'state ok'}>
+                            {!task.state ? <Icon icon={tarefaPendente}/> : <Icon icon={tarefaConcluida}/>} 
+                        </div>
+                        <div className="description">
+                            {task.description} 
+                        </div>
+                    </div>
+                    <div className="delete">
+                        <Icon icon={lixeira}/> 
+                    </div>
+                </li>
+                )
+            })
+        );
+    }
+
     return(
         <div className="TodoApp">
             <div className="titles">To do - App</div>
@@ -9,16 +59,13 @@ const Home = props => {
                 <div className="newTodo">
                     <form onSubmit={false}>
                         <input type="text" placeholder="Tarefa..."/>
-                        <button type="button">+</button>
+                        <button type="button"><Icon icon={adicionarTarefa}/></button>
                     </form>
                 </div>
 
                 <div className="list">
                     <ul>
-                        <li>Tarefa 1</li>
-                        <li>Tarefa 2</li>
-                        <li>Tarefa 3</li>
-                        <li>Tarefa 4</li>
+                        {renderTarefas()}
                     </ul>
                 </div>
             </div>
